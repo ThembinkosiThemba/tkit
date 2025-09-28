528fa3eeba193933ccf6c5ab753e7f7a513a3e4b
ThembinkosiThemba <thembinkosimkhonta01@gmail.com>
2025-09-28 08:47:01 +0200

    fix: version tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -18,7 +18,7 @@ fn test_version_command() {
     cmd.arg("--version");
     cmd.assert()
         .success()
-        .stdout(predicate::str::contains("0.1.0"));
+        .stdout(predicate::str::contains("0.1.1"));
 }
 
 #[test]
